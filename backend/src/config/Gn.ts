8c9f729b08a934d6ee603b4c74c7a498496bbed9
Alan Martines <alancpmartines@hotmail.com>
2023-09-26 01:26:46 -0400

    Fix GERENCIANET_SANDBOX

--- backend/src/config/Gn.ts
+++ backend/src/config/Gn.ts
@@ -6,7 +6,7 @@ const cert = path.join(
 );
 
 export = {
-  sandbox: process.env.GERENCIANET_SANDBOX,
+  sandbox: process.env.GERENCIANET_SANDBOX as string,
   client_id: process.env.GERENCIANET_CLIENT_ID as string,
   client_secret: process.env.GERENCIANET_CLIENT_SECRET as string,
   pix_cert: cert
