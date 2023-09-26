e660061fd71b7be6aeda6bf375c1b23a87adacc0
Alan Martines <alancpmartines@hotmail.com>
2023-09-26 01:32:42 -0400

    Fix

--- backend/src/config/Gn.ts
+++ backend/src/config/Gn.ts
@@ -4,9 +4,9 @@ const cert = path.join(
   __dirname,
   `../../certs/${process.env.GERENCIANET_PIX_CERT}.p12`
 );
-
+let isCheck = process.env.GERENCIANET_CLIENT_ID == 'true' ? true : false;
 export = {
-  sandbox: true,
+  sandbox: isCheck,
   client_id: process.env.GERENCIANET_CLIENT_ID as string,
   client_secret: process.env.GERENCIANET_CLIENT_SECRET as string,
   pix_cert: cert
