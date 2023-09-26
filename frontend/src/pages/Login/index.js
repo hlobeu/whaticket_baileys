7033144a3b916570f6028ce45697eaceb1eb2795
Alan Martines <alancpmartines@hotmail.com>
2023-09-26 14:22:59 -0400

    Update

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -16,17 +16,20 @@ import { i18n } from "../../translate/i18n";
 import { AuthContext } from "../../context/Auth/AuthContext";
 import logoDefault from "../../assets/logoLoginOption.png";
 const logo = process.env.REACT_APP_LOGO_LOGIN || logoDefault;
+const copyright = process.env.REACT_APP_COPYRIGHT || "";
+const copyrightYear = process.env.REACT_APP_COPYRIGHT_YEAR || "0000";
+const copyrightUrl = process.env.REACT_APP_COPYRIGHT_URL || "";
 
 
 const Copyright = () => {
 	return (
 		<Typography variant="body2" color="textSecondary" align="center">
-			{"Copyleft "}
-			<Link color="inherit" href="#">
-				---
+			{"Copyright © "}{copyrightYear}{"- "}
+			<Link color="inherit" href={copyrightUrl}>
+				{copyright}
 			</Link>{" "}
 			{new Date().getFullYear()}
-			{"."}
+			{" EletroInfo - Desenvolvido por Alan Martines."}
 		</Typography>
 	);
 };
