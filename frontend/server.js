a64d820295bcc61f1ca32f7d34b7259507d7b2fc
Alan Martines <alancpmartines@hotmail.com>
2023-09-26 00:28:52 -0400

    Add PORT parameter

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const { logger } = require("./src/utils/logger");
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(3000);
+app.listen(process.env.PORT, async () => {
+  logger.info(`Server started on port: ${process.env.PORT}`);
+});
 
